96432856b6bb534b92be5a7fa24bc0a666590808
aQuafr3sh <aquafr3sh@gmail.com>
2021-11-16 22:01:07 +0200

    For loop in main function added to create sub-folders of each CSV file found in specified folder

--- main.py
+++ main.py
@@ -47,13 +47,22 @@ boy_girl = ""
 assignment_name = ""
 student_name = ""
 
-# # Main function
-# def main():
-#     # TODO: add main function code
 
-# TODO: Break script into manageable helper functions
+# Main function
+# TODO: add main function code
+def main():
+    # Iterate through each CSV file in die CSV directory and create a new directory
+    # where comments will be stored based on the CSV file name
+    for file in Path(csv_dir).glob("*.csv"):
+        with open(file, encoding="ISO-8859-1", mode='r') as csv_file:
+            class_path = file.name[:-4]
+            if not os.path.exists(txt_dir + f"\\{class_path}"):
+                os.mkdir(txt_dir + f"\\{class_path}")
+
+
 # Helper Functions
-
+# TODO: Break script into manageable helper functions
+# TODO:  Create function to parse Excel filename to determine subject
 
 # Calls a random line from the chosen comment file.
 # Formatted variables are defined for interaction within text file
@@ -70,156 +79,155 @@ def random_line(fname):
                                            assignment_name=assignment_name)
 
 
-# Iterate through each CSV file in die CSV directory and create a new directory where comments will be stored
-# based on the CSV file name
-for file in Path(csv_dir).glob("*.csv"):
-    with open(file, encoding="ISO-8859-1", mode='r') as csv_file:
-        class_path = file.name[:-4]
-        if not os.path.exists(txt_dir + f"\\{class_path}"):
-            os.mkdir(txt_dir + f"\\{class_path}")
+# # Iterate through each CSV file in die CSV directory and create a new directory where comments will be stored
+# # based on the CSV file name
+# for file in Path(csv_dir).glob("*.csv"):
+#     with open(file, encoding="ISO-8859-1", mode='r') as csv_file:
+#         class_path = file.name[:-4]
+#         if not os.path.exists(txt_dir + f"\\{class_path}"):
+#             os.mkdir(txt_dir + f"\\{class_path}")
+#
+#         # Create two lists from the CSV files
+#         csv_reader = csv.reader(csv_file)
+#         fields = csv_reader.__next__()  # isolates fields from the rest of the data
+#         data_list = list(csv_reader)  # Create list of CSV file
+#         # The number index is important - it is the last field before assignments are listed
+#         # Use this index to determine the indexes of all other assignments
+#         number_index = fields.index("Number")
+#         first_assessment = fields.index("Number") + 1
+#         final_index = fields.index("FINAL")
+#
+#         # General performance comments
+#         for row in data_list:
+#             with open(txt_dir + f"\\{class_path}" + f"\\{row[1]}_{row[2]}_{row[number_index]}.txt", "w") as text_file:
+#                 student_name = row[2]
+#                 if str(row[-1]) == "A":
+#                     text_file.write("!!!NO FINAL MARK!!! - ")
+#                 elif float(row[-1]) < .4:
+#                     text_file.write(random_line(fail_file))
+#                 elif float(row[-1]) < .5:
+#                     text_file.write(random_line(careful_file))
+#                 elif float(row[-1]) < .6:
+#                     text_file.write(random_line(satisfactory_file))
+#                 elif float(row[-1]) < .8:
+#                     text_file.write(random_line(good_file))
+#                 else:
+#                     text_file.write(random_line(excellent_file))
+#
+#         # Failed assignment comments
+#         student_count = 0
+#         while student_count < len(data_list):
+#             single_student = (data_list[student_count])
+#             assignment_count = first_assessment
+#             while assignment_count < final_index:
+#                 assignment_name = fields[assignment_count]
+#                 student_name = single_student[2]
+#                 He_She = "He" if str(single_student[3]).upper() == "M" else "She"
+#                 he_she = "he" if str(single_student[3]).upper() == "M" else "she"
+#                 him_her = "him" if str(single_student[3]).upper() == "M" else "her"
+#                 his_her = "his" if str(single_student[3]).upper() == "M" else "her"
+#                 His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
+#                 if str(single_student[assignment_count]) == "A":
+#                     pass
+#                 elif float(single_student[assignment_count]) < .4:
+#                     with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
+#                                                             f"{single_student[number_index]}.txt", 'a+') as text_file:
+#                         text_file.write(random_line(assessment_file))
+#                 assignment_count += 1
+#             student_count += 1
+#
+#         # Figure out how to let the functions work - later
+#         # category_comment(4, pleasure_file)
+#         # category_comment(5, attention_file)
+#         # category_comment(6, disrupt_file)
+#         # category_comment(7, reading_file)
+#
+#         # Other observation comments
+#         student_count = 0
+#         while student_count < len(data_list):
+#             single_student = (data_list[student_count])
+#             student_name = single_student[2]
+#             He_She = "He" if str(single_student[3]).upper() == "M" else "She"
+#             he_she = "he" if str(single_student[3]).upper() == "M" else "she"
+#             him_her = "him" if str(single_student[3]).upper() == "M" else "her"
+#             his_her = "his" if str(single_student[3]).upper() == "M" else "her"
+#             His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
+#             boy_girl = "boy" if str(single_student[3]).upper() == "M" else "girl"
+#             if str(single_student[4]).upper() == "X":
+#                 with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
+#                                                         f"{single_student[number_index]}.txt", 'a+') as text_file:
+#                     text_file.write(random_line(pleasure_file))
+#             student_count += 1
+#
+#             student_count = 0
+#             while student_count < len(data_list):
+#                 single_student = (data_list[student_count])
+#                 student_name = single_student[2]
+#                 He_She = "He" if str(single_student[3]).upper() == "M" else "She"
+#                 he_she = "he" if str(single_student[3]).upper() == "M" else "she"
+#                 him_her = "him" if str(single_student[3]).upper() == "M" else "her"
+#                 his_her = "his" if str(single_student[3]).upper() == "M" else "her"
+#                 His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
+#                 boy_girl = "boy" if str(single_student[3]).upper() == "M" else "girl"
+#                 if str(single_student[5]).upper() == "X":
+#                     with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
+#                                                             f"{single_student[number_index]}.txt", 'a+') as text_file:
+#                         text_file.write(random_line(attention_file))
+#                 student_count += 1
+#
+#             student_count = 0
+#             while student_count < len(data_list):
+#                 single_student = (data_list[student_count])
+#                 student_name = single_student[2]
+#                 He_She = "He" if str(single_student[3]).upper() == "M" else "She"
+#                 he_she = "he" if str(single_student[3]).upper() == "M" else "she"
+#                 him_her = "him" if str(single_student[3]).upper() == "M" else "her"
+#                 his_her = "his" if str(single_student[3]).upper() == "M" else "her"
+#                 His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
+#                 boy_girl = "boy" if str(single_student[3]).upper() == "M" else "girl"
+#                 if str(single_student[6]).upper() == "X":
+#                     with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
+#                                                             f"{single_student[number_index]}.txt", 'a+') as text_file:
+#                         text_file.write(random_line(disrupt_file))
+#                 student_count += 1
+#
+#             student_count = 0
+#             while student_count < len(data_list):
+#                 single_student = (data_list[student_count])
+#                 student_name = single_student[2]
+#                 He_She = "He" if str(single_student[3]).upper() == "M" else "She"
+#                 he_she = "he" if str(single_student[3]).upper() == "M" else "she"
+#                 him_her = "him" if str(single_student[3]).upper() == "M" else "her"
+#                 his_her = "his" if str(single_student[3]).upper() == "M" else "her"
+#                 His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
+#                 boy_girl = "boy" if str(single_student[3]).upper() == "M" else "girl"
+#                 if str(single_student[7]).upper() == "X":
+#                     with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
+#                                                             f"{single_student[number_index]}.txt", 'a+') as text_file:
+#                         text_file.write(random_line(reading_file))
+#                 student_count += 1
+#
+# # Write outputs to an excel file and delete the intermediate TXT data
+# subfolder_name = [f.name for f in os.scandir(txt_dir) if f.is_dir()]
+# subfolder_path = [f.path for f in os.scandir(txt_dir) if f.is_dir()]
+#
+# sub_index = 0
+# while sub_index < len(subfolder_name):
+#     row = 0
+#     col = 0
+#     workbook = xlsxwriter.Workbook(subfolder_path[sub_index] + f"\\{subfolder_name[sub_index]}.xlsx")
+#     worksheet = workbook.add_worksheet()
+#     worksheet.set_column("A:A", 40)
+#     worksheet.set_column("B:B", 200)
+#     for file in Path(subfolder_path[sub_index]).glob("*.txt"):
+#         with open(file, encoding="ISO-8859-1", mode="r") as txt_file:
+#             worksheet.write(row, col, file.name[:-4])
+#             worksheet.write(row, col + 1, file.read_text())
+#             row += 1
+#         os.remove(file)
+#     workbook.close()
+#     sub_index += 1
 
-        # Create two lists from the CSV files
-        csv_reader = csv.reader(csv_file)
-        fields = csv_reader.__next__()  # isolates fields from the rest of the data
-        data_list = list(csv_reader)  # Create list of CSV file
-        # The number index is important - it is the last field before assignments are listed
-        # Use this index to determine the indexes of all other assignments
-        number_index = fields.index("Number")
-        first_assessment = fields.index("Number") + 1
-        final_index = fields.index("FINAL")
-
-        # General performance comments
-        for row in data_list:
-            with open(txt_dir + f"\\{class_path}" + f"\\{row[1]}_{row[2]}_{row[number_index]}.txt", "w") as text_file:
-                student_name = row[2]
-                if str(row[-1]) == "A":
-                    text_file.write("!!!NO FINAL MARK!!! - ")
-                elif float(row[-1]) < .4:
-                    text_file.write(random_line(fail_file))
-                elif float(row[-1]) < .5:
-                    text_file.write(random_line(careful_file))
-                elif float(row[-1]) < .6:
-                    text_file.write(random_line(satisfactory_file))
-                elif float(row[-1]) < .8:
-                    text_file.write(random_line(good_file))
-                else:
-                    text_file.write(random_line(excellent_file))
-
-        # Failed assignment comments
-        student_count = 0
-        while student_count < len(data_list):
-            single_student = (data_list[student_count])
-            assignment_count = first_assessment
-            while assignment_count < final_index:
-                assignment_name = fields[assignment_count]
-                student_name = single_student[2]
-                He_She = "He" if str(single_student[3]).upper() == "M" else "She"
-                he_she = "he" if str(single_student[3]).upper() == "M" else "she"
-                him_her = "him" if str(single_student[3]).upper() == "M" else "her"
-                his_her = "his" if str(single_student[3]).upper() == "M" else "her"
-                His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
-                if str(single_student[assignment_count]) == "A":
-                    pass
-                elif float(single_student[assignment_count]) < .4:
-                    with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
-                                                            f"{single_student[number_index]}.txt", 'a+') as text_file:
-                        text_file.write(random_line(assessment_file))
-                assignment_count += 1
-            student_count += 1
-
-        # Figure out how to let the functions work - later
-        # category_comment(4, pleasure_file)
-        # category_comment(5, attention_file)
-        # category_comment(6, disrupt_file)
-        # category_comment(7, reading_file)
-
-        # Other observation comments
-        student_count = 0
-        while student_count < len(data_list):
-            single_student = (data_list[student_count])
-            student_name = single_student[2]
-            He_She = "He" if str(single_student[3]).upper() == "M" else "She"
-            he_she = "he" if str(single_student[3]).upper() == "M" else "she"
-            him_her = "him" if str(single_student[3]).upper() == "M" else "her"
-            his_her = "his" if str(single_student[3]).upper() == "M" else "her"
-            His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
-            boy_girl = "boy" if str(single_student[3]).upper() == "M" else "girl"
-            if str(single_student[4]).upper() == "X":
-                with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
-                                                        f"{single_student[number_index]}.txt", 'a+') as text_file:
-                    text_file.write(random_line(pleasure_file))
-            student_count += 1
-
-            student_count = 0
-            while student_count < len(data_list):
-                single_student = (data_list[student_count])
-                student_name = single_student[2]
-                He_She = "He" if str(single_student[3]).upper() == "M" else "She"
-                he_she = "he" if str(single_student[3]).upper() == "M" else "she"
-                him_her = "him" if str(single_student[3]).upper() == "M" else "her"
-                his_her = "his" if str(single_student[3]).upper() == "M" else "her"
-                His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
-                boy_girl = "boy" if str(single_student[3]).upper() == "M" else "girl"
-                if str(single_student[5]).upper() == "X":
-                    with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
-                                                            f"{single_student[number_index]}.txt", 'a+') as text_file:
-                        text_file.write(random_line(attention_file))
-                student_count += 1
-
-            student_count = 0
-            while student_count < len(data_list):
-                single_student = (data_list[student_count])
-                student_name = single_student[2]
-                He_She = "He" if str(single_student[3]).upper() == "M" else "She"
-                he_she = "he" if str(single_student[3]).upper() == "M" else "she"
-                him_her = "him" if str(single_student[3]).upper() == "M" else "her"
-                his_her = "his" if str(single_student[3]).upper() == "M" else "her"
-                His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
-                boy_girl = "boy" if str(single_student[3]).upper() == "M" else "girl"
-                if str(single_student[6]).upper() == "X":
-                    with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
-                                                            f"{single_student[number_index]}.txt", 'a+') as text_file:
-                        text_file.write(random_line(disrupt_file))
-                student_count += 1
-
-            student_count = 0
-            while student_count < len(data_list):
-                single_student = (data_list[student_count])
-                student_name = single_student[2]
-                He_She = "He" if str(single_student[3]).upper() == "M" else "She"
-                he_she = "he" if str(single_student[3]).upper() == "M" else "she"
-                him_her = "him" if str(single_student[3]).upper() == "M" else "her"
-                his_her = "his" if str(single_student[3]).upper() == "M" else "her"
-                His_Her = "His" if str(single_student[3]).upper() == "M" else "Her"
-                boy_girl = "boy" if str(single_student[3]).upper() == "M" else "girl"
-                if str(single_student[7]).upper() == "X":
-                    with open(txt_dir + f"\\{class_path}" + f"\\{single_student[1]}_{single_student[2]}_"
-                                                            f"{single_student[number_index]}.txt", 'a+') as text_file:
-                        text_file.write(random_line(reading_file))
-                student_count += 1
-
-# Write outputs to an excel file and delete the intermediate TXT data
-subfolder_name = [f.name for f in os.scandir(txt_dir) if f.is_dir()]
-subfolder_path = [f.path for f in os.scandir(txt_dir) if f.is_dir()]
-
-sub_index = 0
-while sub_index < len(subfolder_name):
-    row = 0
-    col = 0
-    workbook = xlsxwriter.Workbook(subfolder_path[sub_index] + f"\\{subfolder_name[sub_index]}.xlsx")
-    worksheet = workbook.add_worksheet()
-    worksheet.set_column("A:A", 40)
-    worksheet.set_column("B:B", 200)
-    for file in Path(subfolder_path[sub_index]).glob("*.txt"):
-        with open(file, encoding="ISO-8859-1", mode="r") as txt_file:
-            worksheet.write(row, col, file.name[:-4])
-            worksheet.write(row, col + 1, file.read_text())
-            row += 1
-        os.remove(file)
-    workbook.close()
-    sub_index += 1
-
-# TODO: Uncomment once main function works
-# # Run main program
-# if __name__ == "__main__":
-#     main()
+# Run main program
+if __name__ == "__main__":
+    main()
