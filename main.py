dcb9462b9826283781c9fb41d6f89f04381744b8
aQuafr3sh <aquafr3sh@gmail.com>
2021-11-25 21:53:17 +0200

    Capitalized global variables to make their use more obvious. Added less verbose comments. csv_to_dataframe function to read CSV file into dataframe. validata_output_directory function to validate existence of output directories and create them if they do not exist. Various gender pronoun functions to clean up code in main function. Returns pronouns based on the gender marked in the input csv file. gen_eng_fal function: This function generates random subject comment output for the final mark of English First additional language students and writes the output to a text file saved in the relevant class output directory.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Created by:    Philip van Schalkwyk
 Contact:       philiplvans@gmail.com
-Last updated:  2021-11-23
+Last updated:  2021-11-25
 
 This script is used to automatically generate subject comments for English
 first additional language students, based on:
@@ -22,22 +22,22 @@ import xlsxwriter
 
 # Variables for all the different files and folders that will be used to read and write data
 # Set the current working directory to the folder in which the file is contained
-cwd = os.getcwd()
-csv_dir = cwd + r"\csv"
-txt_dir = cwd + r"\comment_output"
-comment_dir = cwd + r"\comment_input"
+CWD = os.getcwd()
+CSV_DIR = CWD + r"\csv"
+TXT_DIR = CWD + r"\comment_output"
+COMMENT_DIR = CWD + r"\comment_input"
 
 # File Variables
-fail_f = comment_dir + r"\1_fail.txt"
-care_f = comment_dir + r"\2_careful.txt"
-satisfied_f = comment_dir + r"\3_satisfactory.txt"
-good_f = comment_dir + r"\4_good.txt"
-excel_f = comment_dir + r"\5_excellent.txt"
-ass_f = comment_dir + r"\6_assessmentfail.txt"
-pleasure_f = comment_dir + r"\7_pleasure.txt"
-attention_f = comment_dir + r"\8_attention.txt"
-disrupt_f = comment_dir + r"\9_disrupt.txt"
-read_f = comment_dir + r"\10_read.txt"
+FAIL_F = COMMENT_DIR + r"\1_fail.txt"
+CARE_F = COMMENT_DIR + r"\2_careful.txt"
+SATIS_F = COMMENT_DIR + r"\3_satisfactory.txt"
+GOOD_F = COMMENT_DIR + r"\4_good.txt"
+EXCEL_F = COMMENT_DIR + r"\5_excellent.txt"
+ASS_F = COMMENT_DIR + r"\6_assessmentfail.txt"
+PLEASURE_F = COMMENT_DIR + r"\7_pleasure.txt"
+ATT_F = COMMENT_DIR + r"\8_attention.txt"
+DISRUPT_F = COMMENT_DIR + r"\9_disrupt.txt"
+READ_F = COMMENT_DIR + r"\10_read.txt"
 
 # Main function
 # TODO: add main function code
@@ -48,52 +48,92 @@ read_f = comment_dir + r"\10_read.txt"
 
 
 def main():
-    for file in Path(csv_dir).glob("*.csv"):
+    # Iterate through CSV files in input directory
+    for file in Path(CSV_DIR).glob("*.csv"):
         class_path = file.name[:-4]
-        df = pd.read_csv(file, header=0, encoding="ISO-8859-1")
+        df = csv_to_dataframe(file)
 
+        # Create variables to point to the locations of the first assignment and the final mark
         first_assignment = int(df.columns.get_loc("Number"))+1
         final_index = int(df.columns.get_loc("FINAL"))
 
-        if not os.path.exists(f"{txt_dir}\\{class_path}"):
-            os.mkdir(f"{txt_dir}\\{class_path}")
+        # Check whether output directory exists, create it if it does not exist
+        validate_output_directory(TXT_DIR, class_path)
 
+        # Iterate through the data in the Pandas dataframe and create subject comments based on specified criteria
         for row in df.itertuples(index=False):
-            with open(f"{txt_dir}\\{class_path}\\{row.Surname}_{row.Nickname}_{row.Number}.txt", "w") as text_file:
+            with open(f"{TXT_DIR}\\{class_path}\\{row.Surname}_{row.Nickname}_{row.Number}.txt", "w") as text_file:
                 sname = row.Nickname
-                # Check gender of student
-                if str(row.Sex).upper() == "M":
-                    he_she = "he"
-                    He_She = "He"
-                    him_her = "him"
-                    his_her = "his"
-                    His_Her = "His"
-                    boy_girl = "boy"
-                else:
-                    he_she = "she"
-                    He_She = "She"
-                    him_her = "her"
-                    his_her = "her"
-                    His_Her = "Her"
-                    boy_girl = "girl"
+                # Check gender of student and assign correct pronouns
+                boy_girl = pn_boy_girl(str(row.Sex).upper())
+                he_she = pn_he_she(str(row.Sex).upper())
+                He_She = pn_He_She(str(row.Sex).upper())
+                him_her = pn_him_her(str(row.Sex).upper())
+                his_her = pn_his_her(str(row.Sex).upper())
+                His_Her = pn_His_Her(str(row.Sex).upper())
 
-                if str(row.FINAL) == "A":
-                    text_file.write("!!!NO FINAL MARK!!! - ")
-                elif float(row.FINAL) < .4:
-                    text_file.write(rand_line(fail_f, sname, he_she, He_She, him_her, his_her, His_Her, boy_girl))
-                elif float(row.FINAL) < .5:
-                    text_file.write(rand_line(care_f, sname, he_she, He_She, him_her, his_her, His_Her, boy_girl))
-                elif float(row.FINAL) < .6:
-                    text_file.write(rand_line(satisfied_f, sname, he_she, He_She, him_her, his_her, His_Her, boy_girl))
-                elif float(row.FINAL) < .8:
-                    text_file.write(rand_line(good_f, sname, he_she, He_She, him_her, his_her, His_Her, boy_girl))
-                else:
-                    text_file.write(rand_line(excel_f, sname, he_she, He_She, him_her, his_her, His_Her, boy_girl))
+                gen_eng_fal(row.FINAL, text_file, sname, he_she, He_She, him_her, his_her, His_Her, boy_girl)
 
 
 # Helper Functions
 # TODO: Break script into manageable helper functions
 # TODO:  Create function to parse Excel filename to determine subject
+
+# Several functions to output pronouns based on gender marked in spreadsheet
+def pn_boy_girl(gender_p):
+    return "boy" if gender_p == 'M' else "girl"
+
+
+def pn_he_she(gender_p):
+    return "he" if gender_p == 'M' else "she"
+
+
+def pn_He_She(gender_p):
+    return "He" if gender_p == 'M' else "She"
+
+
+def pn_him_her(gender_p):
+    return "him" if gender_p == 'M' else "her"
+
+
+def pn_his_her(gender_p):
+    return "his" if gender_p == 'M' else "her"
+
+
+def pn_His_Her(gender_p):
+    return "His" if gender_p == 'M' else "Her"
+
+
+# Read CSV file into a dataframe, top row is the header, encoding is used because of symbols
+# such as ô, é, and ê.
+def csv_to_dataframe(csv_file_p):
+    df = pd.read_csv(csv_file_p, header=0, encoding="ISO-8859-1")
+    return df
+
+
+# Validate the existence of the output folders, if non-existent, create them
+def validate_output_directory(txt_dir_p, class_path_p):
+    if not os.path.exists(f"{txt_dir_p}\\{class_path_p}"):
+        os.mkdir(f"{txt_dir_p}\\{class_path_p}")
+        print(f"Created directory: {txt_dir_p}\\{class_path_p}")
+
+
+# Reads the final mark of the student and writes general subject comments
+# to a text file depending on the mark achieved.
+def gen_eng_fal(f_mark_p, txt_f_p, sname_p, he_she_p, He_She_p, him_her_p, his_her_p, His_Her_p, boy_girl_p):
+    if str(f_mark_p) == "A":
+        txt_f_p.write("!!!NO FINAL MARK!!! - ")
+    elif float(f_mark_p) < .4:
+        txt_f_p.write(rand_line(FAIL_F, sname_p, he_she_p, He_She_p, him_her_p, his_her_p, His_Her_p, boy_girl_p))
+    elif float(f_mark_p) < .5:
+        txt_f_p.write(rand_line(CARE_F, sname_p, he_she_p, He_She_p, him_her_p, his_her_p, His_Her_p, boy_girl_p))
+    elif float(f_mark_p) < .6:
+        txt_f_p.write(rand_line(SATIS_F, sname_p, he_she_p, He_She_p, him_her_p, his_her_p, His_Her_p, boy_girl_p))
+    elif float(f_mark_p) < .8:
+        txt_f_p.write(rand_line(GOOD_F, sname_p, he_she_p, He_She_p, him_her_p, his_her_p, His_Her_p, boy_girl_p))
+    else:
+        txt_f_p.write(rand_line(EXCEL_F, sname_p, he_she_p, He_She_p, him_her_p, his_her_p, His_Her_p, boy_girl_p))
+
 
 # Calls a random line from the chosen comment file.
 # Formatted variables are defined for interaction within text file
